2d995edaa89f0b062b007338a877310924406351
cyclope205 <71901181+cyclope205@users.noreply.github.com>
2026-09-03 23:34:30 +0200

    Bump CARD_VERSION from 2.2.3 to 2.2.4

--- custom_components/programme_tnt_fr/__init__.py
+++ custom_components/programme_tnt_fr/__init__.py
@@ -20,7 +20,7 @@ PLATFORMS = ["sensor"]
 
 CARD_FILENAME = "programme-tnt-fr-card.js"
 CARD_URL_PATH = f"/programme_tnt_fr/{CARD_FILENAME}"
-CARD_VERSION = "2.2.3"
+CARD_VERSION = "2.2.4"
 _CARD_REGISTERED_KEY = f"{DOMAIN}_card_registered"
 _WS_API_REGISTERED_KEY = f"{DOMAIN}_ws_api_registered"
 
